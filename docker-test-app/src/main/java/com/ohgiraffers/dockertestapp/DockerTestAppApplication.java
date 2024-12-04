1e96b5b4c494a8d17f98b23afff9e1f9dbd722d5
nicdkim <zzsehdrb@naver.com>
2024-12-04 10:56:19 +0900

    first commit

---
+++ docker-test-app/src/main/java/com/ohgiraffers/dockertestapp/DockerTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.dockertestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerTestAppApplication.class, args);
+    }
+
+}
